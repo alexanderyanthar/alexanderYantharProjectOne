b832b3ab0c3c076288db24c2574dd1535317d4e9
alexanderyanthar <alexander.yanthar@outlook.com>
2023-05-04 21:36:47 -0400

    added js functionality to footer

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,6 +2,8 @@
 const hamburger = document.querySelector(".hamburgerMenu");
 const navMenu = document.querySelector(".navMenu");
 let lastScrollY = window.scrollY;
+const contact = document.querySelector('.contact');
+const userProfile = document.querySelector('.userProfile');
 
 // Function to hide or show navbar on scroll
 
@@ -13,6 +15,15 @@ To build a functional hamburger menu/responsive navbar:
 https://www.youtube.com/watch?v=At4B7A4GOPg&t=2s&ab_channel=WebDevSimplified
 */
 
+
+// function to open contact menu
+contact.addEventListener('click', function(){
+    contact.classList.toggle('active');
+});
+
+userProfile.addEventListener('click', function(){
+    userProfile.classList.toggle('active');
+})
 
 // Functions to close hamburger menu
 const showMenu = () => {
