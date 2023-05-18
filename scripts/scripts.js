ba8cf3efe5f668d08e16f1f69eeac8820edbead7
alexanderyanthar <alexander.yanthar@outlook.com>
2023-05-18 19:09:59 -0400

    added cart icon and counter

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,23 +1,15 @@
-// Variables to target hamburger menu and nav bar
 const hamburger = document.querySelector(".hamburgerMenu");
 const navMenu = document.querySelector(".navMenu");
 let lastScrollY = window.scrollY;
 const contact = document.querySelector('.contact');
 const userProfile = document.querySelector('.userProfile');
 const modalBackground = document.querySelector('.modalBackground');
-const submitBtn = document.querySelector('#submit[type=submit]');
+const submitBtn = document.querySelector('#submitButton');
 const form = document.querySelector('form');
 const emailInput = document.getElementById("email");
-// Get the modal
-var modal = document.getElementById("newsletterModal");
+const modal = document.querySelector(".modal");
+const span = document.getElementsByClassName("close")[0];
 
-// Get the button that opens the modal
-var btn = document.getElementById("newsletterBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// Function to hide or show navbar on scroll
 
 /* resources
 To make the navbar hide whenever the user scrolls:
@@ -28,7 +20,6 @@ https://www.youtube.com/watch?v=At4B7A4GOPg&t=2s&ab_channel=WebDevSimplified
 */
 
 
-// function to open contact menu
 contact.addEventListener('click', function(){
     contact.classList.toggle('active');
 });
@@ -37,7 +28,6 @@ userProfile.addEventListener('click', function(){
     userProfile.classList.toggle('active');
 })
 
-// Functions to close hamburger menu
 const showMenu = () => {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
@@ -48,7 +38,6 @@ const closeMenu = () => {
     navMenu.classList.remove('active');
 }
 
-// event listeners to actively open and close menu
 window.addEventListener('scroll', () => {
     if (lastScrollY < window.scrollY) {
         closeMenu();
@@ -63,39 +52,24 @@ document.querySelectorAll(".navLink").forEach(n => n.addEventListener('click', c
 // ** THE CODE BELOW HAS BEEN LARGELY MADE WITH CHATGPT!
 
 
-// Check if the modal has been displayed before
-// Check if the newsletter has already been displayed
 const value = localStorage.getItem('newsletterDisplayed');
 
 if (!localStorage.getItem('newsletterDisplayed')) {
 
-    // Get the halfway point of the page
     const halfHeight = Math.floor(document.body.scrollHeight / 2);
 
-    // Check if the user has scrolled past the halfway point
     window.addEventListener('scroll', function () {
         if (window.pageYOffset >= halfHeight) {
 
-            // Display the newsletter
-            // ...
             modal.style.display = "block";
             modalBackground.style.display = "block";
 
 
-            // When the user clicks on <span> (x), close the modal
             span.onclick = function () {
                 modal.style.display = "none";
                 modalBackground.style.display = "none";
             };
 
-            window.onclick = function (event) {
-                if (event.target == modal) {
-                    modal.style.display = "none";
-                    modalBackground.style.display = "none";
-                }
-            };
-
-            // Set the flag indicating that the newsletter has been displayed
             localStorage.setItem('newsletterDisplayed', true);
 
             emailInput.addEventListener('input', function () {
@@ -109,25 +83,21 @@ if (!localStorage.getItem('newsletterDisplayed')) {
                 }
             });
 
-
-            // In the else part of the function, change what happens by adding a class to the modal display which would hide the previous modal and show the new text.
             form.addEventListener('submit', function(e) {
-                // e.preventDefault();
                 const email = this.value.trim();
                 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
                 
 
                 if (!emailRegex.test(email)) {
                     emailInput.style.border = '2px solid red';
+                    // can't get the else code to work
                 } else {
-                    modal.style.display = 'none';
-                    modalBackground.style.display = 'none';
+                    modalContent.innerHTML = '<h2>Thank you for subscribing!</h2><p>You are now subscribed to our newsletter.</p>';
+
+                    submitBtn.style.display = 'none';
                 }
             });
 
-            // Prevent form submission unless all required inputs are filled
-
-            // Remove the scroll event listener
             window.removeEventListener('scroll', arguments.callee);
         }
     });
